ab32a353f8c8e3e56d728bb8d7b2387e779197f8
jujucinco <52669656+jujucinco@users.noreply.github.com>
2019-09-27 12:26:25 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+window.onload = function() {
+    alert("The window has loaded!");
+    onReady();
+};
+
+function onReady() {
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+
+    addToDoForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let title = newToDoText.value;
+
+        let newLi = document.createElement('li');
+
+        let checkbox = document.createElement('input');
+
+        checkbox.type = "checkbox";
+
+        let deleteBtn = document.createElement('button');
+
+        deleteBtn.textContent = "Delete";
+
+        deleteBtn.addEventListener('click', function(event){
+
+          toDoList.removeChild(this.parentElement);
+        })
+
+        newLi.appendChild(deleteBtn);
+
+        newLi.textContent = title;
+
+        newLi.appendChild(checkbox);
+
+        toDoList.appendChild(newLi);
+
+        newToDoText.value = '';
+
+        
+    });
+
+}
+
